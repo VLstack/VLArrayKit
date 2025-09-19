45291378833c01517922c80a0b5d092f0271d59f
VLstack <github@vlstack.fr>
2025-09-19 11:05:55 +0200

    improve readability and handle optionals

--- Sources/VLArrayKit/median-Double.swift
+++ Sources/VLArrayKit/median-Double.swift
@@ -2,18 +2,25 @@ import Foundation
 
 extension Array where Element == Double
 {
+ @inlinable
  public func median() -> Double?
  {
   guard !self.isEmpty else { return nil }
 
-  let values = self.compactMap { $0 }
-  guard !values.isEmpty else { return nil }
+  let sorted = self.sorted()
+  let mid = self.count / 2
 
-  let sorted = values.sorted()
-  let mid: Int = values.count / 2
-
-  if self.count % 2 == 0 { return ( sorted[mid - 1] + sorted[mid] ) / 2 }
+  if self.count % 2 == 0 { return ( sorted[mid - 1] + sorted[mid] ) / 2  }
 
   return sorted[mid]
+ }
+}
+
+extension Array where Element == Double?
+{
+ @inlinable
+ public func median() -> Double?
+ {
+  self.compactMap { $0 }.median()
  }
 }
